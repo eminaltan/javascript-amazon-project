13e8ee907e59088e5f12b0109b5bc2fab3a5884a
Emin <emin.altan@hotmail.com>
2025-01-08 15:35:50 +0300

    Codes simplfied in money.js

--- script/utils/money.js
+++ script/utils/money.js
@@ -1,13 +1,3 @@
 export function formatCurrency(priceCents) {
-  return (priceCents / 100).toFixed(2);
-}
-
-export function calculatePrice(
-  cart,
-  // product price => default tanımlanmıştır fiyat hesaplaması içindir.
-  calculationType = "product price"
-) {
-  if (calculationType === "product price") {
-    return formatCurrency(cart.productItems.priceCents * cart.quantity);
-  }
+  return (Math.round(priceCents) / 100).toFixed(2);
 }
