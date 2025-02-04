8976b94ef82e029814afea923e983217e7718bd5
emina <emin.altan@hotmail.com>
2025-02-04 14:15:32 +0300

    Added comments to monetTest.js

--- test-jasmine/moneyTest.js
+++ test-jasmine/moneyTest.js
@@ -1,9 +1,14 @@
 import { formatCurrency } from "../script/utils/money.js";
 
+// describe() test suite ismi oluşturmak için kullanılıyor.
 describe("test suite: formatCurrency", () => {
+  // #region bir test case örneği
+  // it() teste isim vermek için kullanılır.
   it("converst cents into dollars", () => {
+    // expect() ile verilen koşul sınanır, koşul doğru ise true değilse false değer döndürür.
     expect(formatCurrency(2095)).toEqual("20.95");
   });
+  //#endregion
 
   it("works with 0", () => {
     expect(formatCurrency(0)).toEqual("0.00");
